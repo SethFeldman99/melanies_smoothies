a26bc041c0e2a118abf49e5362c56f8e9405416b
SethFeldman99 <sethfeldman99@gmail.com>
2024-06-09 12:55:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,12 +43,12 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_orer)
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_orer)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 #    st.write(my_insert_stmt)
 #    st.stop()
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+time_to_insert = st.button('Submit Order')
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!', icon="✅")
+    st.success('Your Smoothie is ordered!', icon="✅")
